b8366146d946124cac17717b477b18b5e94fc8cc
Piyush Mehta <mehtapiyush81@gmail.com>
2021-05-26 23:15:28 +0530

    [Feature] Form Validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 
 @Component({
@@ -10,10 +11,11 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
     this.postCreated.emit(post);
   }
