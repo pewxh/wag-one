778646828b747f9b3f16ea322ec1c381f5ae350d
Piyush <mehtapiyush81@gmail.com>
2021-06-01 08:56:50 +0530

    [Added] Writing of data into DataBase

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post Added Successfuly",
   });
